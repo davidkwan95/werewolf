4252b27b09165540024631057599d0e1e89be4dd
Vincent Theophilus Ciputra <vincent_theophilusc@yahoo.com>
2016-04-30 17:27:07 +0700

    added random werewolf function

--- server/player.js
+++ server/player.js
@@ -15,4 +15,39 @@ class Player{
 	}
 }
 
+Player.prototype.randomWerewolf = function(playerList){
+	var countWerewolf = 0;
+	var roleList = [];
+	var role;
+	for (var i = 0;i<playerList.length();i++){
+		role = Math.random();
+		if (role == 1){
+			if (countWerewolf < 2){
+				roleList.push(role);
+				countWerewolf++;
+			} else {
+				role = 0;
+				roleList.push(role);
+			}
+		} else {
+			if (i == playerList.length()-2 && countWerewolf == 0){
+				role = 1;
+				roleList.push(role);
+				roleList.push(role);
+				countWerewolf += 2;
+				break;
+			} else if (i == playerList.length()-1 && countWerewolf == 1){
+				role = 1;
+				roleList.push(role);
+				countWerewolf++;
+				break;
+			} else {
+				roleList.push(role);
+			}
+		}
+		
+	}
+	return roleList;
+};
+
 module.exports = Player;
